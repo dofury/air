4dddd28f6f4746ff92d9e02df026a03406d2ef5a
dofury <cha_dh1004@naver.com>
2023-01-05 04:36:29 +0900

    get 쿼리 데이터 전송 기능 구현.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const axios = require('axios');
 const https = require('https');
 const express = require('express');
+const util = require('./utility');
 const mysqlConnection = require('./database');
 const app = express();
 const conection = mysqlConnection.init();
+const reqTicket = require('./reqTicket');
 mysqlConnection.open(conection);
 require('dotenv').config();
 
@@ -16,29 +18,45 @@ app.listen(8080, function(){
 
 
 app.get('/air', async function(request, response){
-    axios({
-        method: 'GET',
-        url: 'https://apis.data.go.kr/1613000/DmstcFlightNvgInfoService/getFlightOpratInfoList?',
-        responseType: 'JSON',
-        rejectUnauthorized : false
-    }).then(async function (response){
-        try{
-            const result = await axios.get('http://apis.data.go.kr/1613000/DmstcFlightNvgInfoService/getFlightOpratInfoList?serviceKey='+process.env.SERVICE_KEY,{
-                params: {
-                    pageNo: '1',
-                    numOfRows: '10',
-                    _type: 'json',
-                    depAirportId: 'NAARKPK',
-                    arrAirportId: 'NAARKPC',
-                    depPlandTime: '20230106',
-                    airlineId: 'JNA'
-                }
-            })
-            var items = result.data.response.body.items;
-            console.log(items.item[0]);
-            mysqlConnection.search(conection,items.item[0]);
-        }catch(error){
-            console.log(error);
-        }
-    })
+    var reqData;
+    if(request.query != null)//쿼리 데이터가 null이 아니면 데이터를 넣음
+    {
+        reqData = new reqTicket.ReqTicket(request.query.depName,request.query.arrName,request.query.date);
+    }
+    var data = await mysqlConnection.search(conection, reqData);
+    if(data)
+    {
+        console.log("데이터있어서 보여줌");
+        console.log(data);
+    }
+    else
+    {
+        axios({
+            method: 'GET',
+            url: 'https://apis.data.go.kr/1613000/DmstcFlightNvgInfoService/getFlightOpratInfoList?',
+            responseType: 'JSON',
+            rejectUnauthorized : false
+        }).then(async function (response){
+            try{
+                const result = await axios.get('http://apis.data.go.kr/1613000/DmstcFlightNvgInfoService/getFlightOpratInfoList?serviceKey='+process.env.SERVICE_KEY,{
+                    params: {
+                        pageNo: '1',
+                        numOfRows: '10',
+                        _type: 'json',
+                        depAirportId: util.trans(reqData.depName).toString(),
+                        arrAirportId: util.trans(reqData.arrName).toString(),
+                        depPlandTime: reqData.date,
+                        airlineId: 'JNA'
+                    }
+                })
+                var items = result.data.response.body.items;
+                mysqlConnection.save(conection,items.item[0]);
+                data = await mysqlConnection.search(conection, reqData);
+                console.log("데이터없어서 추가하고 보여줌");
+                console.log(data);
+            }catch(error){
+                console.log(error);
+            }
+        })
+    }
 });
