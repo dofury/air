875b54580ab8b62d84402dfae64fe36ed38cce32
dofury <cha_dh1004@naver.com>
2023-01-19 20:41:35 +0900

    api 응답 속도 개선, respone 결과 최적화, env 암호화

--- server.js
+++ server.js
@@ -8,99 +8,100 @@ const conection = mysqlConnection.init();
 const reqTicket = require('./reqTicket');
 const { json } = require('express');
 mysqlConnection.open(conection);
-const airPorts = ['AAR','ABL','ASV','ESR','FGW','JJA','JNA','KAL','TWB','HGG'];
+const airPorts = ['AAR', 'ABL', 'ASV', 'ESR', 'FGW', 'JJA', 'JNA', 'KAL', 'TWB', 'HGG'];
 require('dotenv').config();
 
 https.globalAgent.options.rejectUnauthorized = false;
 
-app.listen(8080, function(){
+app.listen(8080, function () {
     console.log('listening on 8080');
 });
 
-
-
-app.get('/air', async function(request, response){
+app.get('/air', async function (request, response) {
     let reqData;
-    if(request.query != null)//쿼리 데이터가 null이 아니면 데이터를 넣음
+    if (request.query != null)//쿼리 데이터가 null이 아니면 데이터를 넣음
     {
-        reqData = new reqTicket.ReqTicket(request.query.depName,request.query.arrName,request.query.date);
+        reqData = new reqTicket.ReqTicket(request.query.depName, request.query.arrName, request.query.date);
     }
     let data = await mysqlConnection.search(conection, reqData);
-    var code;
-    if(data)
-    {
-        console.log("데이터 있음");
-        code = {
-            status: 200
-        };
+
+    let result = {
+        data: null,
+        code: {
+            message: null,
+            status: null
+        }
+    };
+
+    if (data) {
+        console.log(result.code.message);
+        result.code.status = "CODE000"
+        result.code.message = "데이터 있음"
     }
-    else
-    {
-    for(const airPort of airPorts)// 항공사가 다르고 같은 날짜 티켓
-    {
-    await axios({
-        method: 'GET',
-        url: 'https://apis.data.go.kr/1613000/DmstcFlightNvgInfoService/getFlightOpratInfoList?',
-        responseType: 'JSON',
-        rejectUnauthorized : false
-    }).then(async function (response){
-        try{
-            const result = await axios.get('https://apis.data.go.kr/1613000/DmstcFlightNvgInfoService/getFlightOpratInfoList?serviceKey='+process.env.SERVICE_KEY,{
-                params: {
-                    pageNo: '1',
-                    numOfRows: '10',
-                    _type: 'json',
-                    depAirportId: util.trans(reqData.depName).toString(),
-                    arrAirportId: util.trans(reqData.arrName).toString(),
-                    depPlandTime: reqData.date,
-                    airlineId: airPort
-                }
-            })
-            let items = result.data.response.body.items;
-            if(items.item != null)
-            {
-                for(let item of items.item)//날짜는 같으나 시간대가 다른 티켓 반복 추가
-                {
-                    try {
-                        mysqlConnection.save(conection,item);  
-                    } catch (error) {
-                        console.log(error);
+    else {
+        await axios({
+            method: 'GET',
+            url: 'https://apis.data.go.kr/1613000/DmstcFlightNvgInfoService/getFlightOpratInfoList?',
+            responseType: 'JSON',
+            rejectUnauthorized: false
+        }).then(async function (response) {
+            try {
+                const result = await axios.get('https://apis.data.go.kr/1613000/DmstcFlightNvgInfoService/getFlightOpratInfoList?serviceKey=' + process.env.SERVICE_KEY, {
+                    params: {
+                        pageNo: '1',
+                        numOfRows: '10',
+                        _type: 'json',
+                        depAirportId: util.trans(reqData.depName).toString(),
+                        arrAirportId: util.trans(reqData.arrName).toString(),
+                        depPlandTime: reqData.date,
+                    }
+                })
+                let items = result.data.response.body.items;
+                if (items.item != null) {
+                    for (let item of items.item)//날짜는 같으나 시간대가 다른 티켓 반복 추가
+                    {
+                        try {
+                            mysqlConnection.save(conection, item);
+                        } catch (error) {
+                            console.log(error);
+                        }
                     }
                 }
-                console.log("데이터없어서 추가");
+            } catch (error) {
+                console.log(error);
+                result.code.status = "CODE200"
+                result.code.message = "예외발생"
             }
-        }catch(error){
-            console.log(error);
+        });
+        
+
+
+        data = await mysqlConnection.search(conection, reqData, function (result) {
+            var dataList = [];
+            for (var data of result) {
+                dataList.push(data);
+            }
+            console.log(dataList);
+        });
+        if (data == 0) {
+            result.code.status = "CODE500"
+            result.code.message = "연계 API에서 데이터를 못 받아옴"
+            console.log(result.code.message);
         }
-    });
-    }
-    data = await mysqlConnection.search(conection, reqData, function (result) {
-        var dataList = [];
-        for (var data of result){
-            dataList.push(data);
+        else {
+            result.code.status = "CODE000"
+            result.code.message = "데이터가 없어서 받아서 저장한 뒤 정상적으로 불러옴"
+            console.log(result.code.message);
         }
-        console.log(dataList);
-    });
-    if(data == 0)
-    {
-        code = {
-            status: 404
-        };
-        console.log('데이터를 받지못했음');
     }
-    else
-    {
-        code = {
-            status: 200
-        };
-        console.log('데이터 추가해서 있음');
-    }
-    }
-    let result = {
-        data: data
-    };
-    JSON.stringify(code);
-    Object.assign(result,code);
-    response.json(result);
+    //CODE000 -> 정상
+    //CODE200 -> 예외 발생
+    //CODE300 -> 서버 점검중
+    //CODE500 -> 연계 API 문제
+    //CODE600 -> 데이터베이스 연결 안됨
     
+    result.data = data
+
+    response.json(result);
+
 });
